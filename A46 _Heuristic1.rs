969ab5a20b888ac012d1055be6274afcf2c1a5fd
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-08 19:44:00 +0900

    Update A46 _Heuristic1.rs

--- A46 _Heuristic1.rs
+++ A46 _Heuristic1.rs
@@ -3,6 +3,59 @@
 #######################################################
 #######################################################
 #######################################################
+use proconio::input;
+use proconio::derive_readable;
+
+#[derive_readable]
+#[derive(Debug, Copy, Clone)]
+struct Point {
+    x: f64, y: f64
+}
+impl Point {
+    fn new(pos_x: f64, pos_y: f64) -> Self {
+        Self{x: pos_x, y: pos_y}
+    }
+}
+// 貪欲法
+fn main() {
+    input!{n: usize, xy: [Point; n]}
+    let mut used: Vec<bool> = vec![false; n];
+    let calc_dist = |i: usize, j: usize| -> f64 {
+        let x_sq = (xy[j].x - xy[i].x) * (xy[j].x - xy[i].x);
+        let y_sq = (xy[j].y - xy[i].y) * (xy[j].y - xy[i].y);
+        (x_sq + y_sq).sqrt()
+    };
+    let mut cur_v: usize = 0;
+    let mut path: Vec<usize> = Vec::new();
+    path.push(cur_v);
+
+    while !used[0] {
+        let mut min_dist: f64 = f64::MAX;
+        let mut nearest_v: usize = n;
+        // avoid the start point
+        for v in 1..n {
+            if cur_v != v && !used[v] {
+                let dist = calc_dist(cur_v, v);
+                if dist < min_dist {
+                    min_dist = dist;
+                    nearest_v = v;
+                }
+            }
+        }
+        // if only 0 remains
+        if nearest_v == n {
+            min_dist = calc_dist(cur_v, 0);
+            nearest_v = 0;
+        }
+        used[nearest_v] = true;
+        path.push(nearest_v);
+        cur_v = nearest_v;
+    }
+
+    for &v in path.iter() {
+        println!("{}", v+1);
+    }
+}
 #######################################################
 use proconio::input;
 use proconio::derive_readable;
